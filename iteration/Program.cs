d6bc0dc79f1c8b4e924358c25f66c20f8dc2b27f
MbuguaDennis <dmbugua.developer@gmail.com>
2026-06-09 23:07:26 +0300

    Feat: C# looping and iteration

---
+++ iteration/Program.cs
@@ -0,0 +1,24 @@
+﻿string userName;
+
+do
+{
+    Console.WriteLine("Enter the name of Software Engineer");
+    userName = Console.ReadLine();
+
+    if (userName != "Dennis")
+    {
+        Console.WriteLine("Oops! Wrong name, try again...");
+    }
+
+} while (userName != "Dennis");
+
+Console.WriteLine("Correct name entered!");
+
+
+
+do
+{
+    Console.WriteLine("I love Eunice Wairimu");
+    userName = Console.ReadLine();
+} while(userName =="Dennis");
+Console.WriteLine("i AM NOTE MARRIED TO ANYONE");
