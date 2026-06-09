8954a9836df054c6fa3eec879a922bf68a1c1339
MbuguaDennis <dmbugua.developer@gmail.com>
2026-06-09 22:46:07 +0300

    Feat: C# collections lecture

---
+++ collections/Program.cs
@@ -0,0 +1,38 @@
+﻿// C# collections
+// arrays
+string  [] fruits = ["bananas","mangoes","apples"];
+
+Console.WriteLine(fruits[0]);
+Console.WriteLine(fruits[1]);
+Console.WriteLine(fruits[2]);
+
+
+int[] myLuckyNumbers = { 3, 6, 9 };
+
+for (int i = 0; i < myLuckyNumbers.Length; i++)
+{
+    Console.WriteLine($"Lucky number #{i + 1} is {myLuckyNumbers[i]}");
+}
+
+// Lists 
+List <string> hobbies = new List<string> ();
+
+hobbies.Add("Reading");
+hobbies.Add("Sleeping");
+hobbies.Add("Preaching");
+Console.WriteLine(hobbies.Count);
+{
+    
+};
+
+
+// Dictiionaries
+
+Dictionary<string, string> countryCapitals = new Dictionary<string, string>
+{
+    { "Kenya", "Nairobi" },
+    { "Uganda", "Kampala" },
+    { "Tanzania", "Dodoma" }
+};
+Console.WriteLine(countryCapitals["Kenya"])
+
---
+++ collections/assignment/Assignment.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Assignment;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Dictionary<string, string> countryCapital = new Dictionary<string, string>
+        {
+            {"Kenya", "Nairobi"},
+            {"Tanzania", "Dar es Salaam"},
+            {"Burundi", "Bujumbura"}
+        };
+
+        foreach (KeyValuePair<string, string> capital in countryCapital)
+        {
+            Console.WriteLine($"{capital.Key}: {capital.Value}");
+        }
+
+        Console.WriteLine("Enter a country's Name");
+        string userInput = Console.ReadLine();
+
+        if (countryCapital.TryGetValue(userInput, out var capitalName))
+        {
+            Console.WriteLine($"The capital city of {userInput} is {capitalName}");
+        }
+        else
+        {
+            Console.WriteLine("Country Not Found!");
+        }
+    }
+}
